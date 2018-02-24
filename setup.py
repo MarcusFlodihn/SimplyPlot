554e5062a0cb2d8cc93ae9fff5051f464e63a9b0
Marcus Flodihn <marcus.flodihn@gmail.com>
2018-02-24 19:03:54 +0100

    Delete setup.py

--- setup.py
+++
@@ -1,9 +0,0 @@
-from distutils.core import setup
-
-setup(name='simply_plot',
-      version='0.1',
-      py_modules=['simply_plot'],
-      install_requires=[
-          'matplotlib',
-      ]
-      )
