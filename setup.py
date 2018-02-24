9a00a4c628b09b406933ac0453954f9d936e175c
Marcus Flodihn <marcus.flodihn@gmail.com>
2018-02-24 19:04:57 +0100

    Update

--- setup.py
+++ setup.py
@@ -1,9 +1,12 @@
-from distutils.core import setup
+from setuptools import setup
 
-setup(name='simply_plot',
-      version='0.1',
-      py_modules=['simply_plot'],
-      install_requires=[
-          'matplotlib',
-      ]
-      )
+setup(
+    name='simplyplot',
+    description='A Python wrapper around Matplotlib, create basic charts with less lines of codes.',
+    version='0.1',
+    license='MIT',
+    py_modules=['simplyplot'],
+    install_requires=['matplotlib'],
+    author="Marcus Flodihn",
+    author_email="marcus.flodihn@gmail.com"
+)
